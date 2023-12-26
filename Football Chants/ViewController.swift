4f294ec3bcbc41b63dcfc1834615dd8b31c0cbaf
Yegor Cheprasov <YegorCheprasov@gmail.com>
2023-12-26 18:18:41 +0300

    First init

---
+++ Football Chants/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Football Chants
+//
+//  Created by Yegor Cheprasov on 26.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
